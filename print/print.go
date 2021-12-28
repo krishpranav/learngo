709f9c2eca905aa1100568ba8311c538070fe13c
krishpranav <krisna.pranav@gmail.com>
2021-12-28 17:39:27 +0530

    print: print funcs

---
+++ print/print.go
@@ -0,0 +1,14 @@
+/* this file should be main to be compiled
+package main
+
+
+import (
+	/* get the module fmt for printing texts */
+	"fmt"
+)
+
+
+/* main function */
+func main() {
+	fmt.Println("Hello, World!!")
+}
